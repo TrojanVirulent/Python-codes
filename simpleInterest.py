f7f4c979d3530864be7fd940d9e60b3cb9eda1ee
swagatikapanda123 <swagatikapanda164@gmail.com>
2019-10-05 21:28:38 +0530

    find simple interest

---
+++ simpleInterest.py
@@ -0,0 +1,16 @@
+# Python3 program to find simple interest 
+# for given principal amount, time and 
+# rate of interest. 
+
+# We can change values here for 
+# different inputs 
+P = 1
+R = 1
+T = 1
+
+# Calculates simple interest 
+SI = (P * R * T) / 100
+
+# Print the resultant value of SI 
+print("simple interest is", SI) 
+
